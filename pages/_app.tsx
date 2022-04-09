fd192c424d4290f8febd7b5f9298c5b2f812d5f0
Dao Zheng <daoyzheng@gmail.com>
2022-04-08 21:41:32 -0600

    Add styled components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
+import styled from 'styled-components'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (

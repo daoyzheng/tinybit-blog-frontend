029e5ce4317d5a5866a6cdab7af6e1c94d4ad474
Dao Zheng <daoyzheng@gmail.com>
2022-04-10 23:20:22 -0600

    Change how query string is passed to strapi api

--- pages/post/[slug]/index.tsx
+++ pages/post/[slug]/index.tsx
@@ -1,22 +1,49 @@
-// import { useRouter } from 'next/router'
-const PostDetails = () => {
-  // const router = useRouter()
-  // const { slug } = router.query
-  return (
+import { IPostItem } from "../../../interfaces/post"
+import { IStrapiDataResponse } from "../../../interfaces/strapi"
+import { getList } from "../../../utils/request"
+import { GetStaticPaths, GetServerSideProps } from 'next'
+import Custom404 from "../../404"
+
+
+interface Props {
+  postDetails: IStrapiDataResponse<IPostItem>
+}
+
+const PostDetails = ({ postDetails }: Props) => {
+  return postDetails ? (
     <div>
       <div className="flex items-center justify-between mb-1">
         <div>Prev post</div>
         <div>Next post</div>
       </div>
       <div className="mt-6 text-center">
-        {/* hleo {slug} */}
+        {postDetails.attributes.title}
       </div>
     </div>
-  )
+  ) : <Custom404/>
 }
 
-// export const getServerSideProps = async (context) => {
-//   // const res = await
-// }
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { locale, params } = context
+  let slug = ''
+  if (params) slug = params.slug as string
+  const query = {
+    filters: {
+      slug: {
+        $eq: slug
+      },
+      locale: {
+        $eq: locale
+      }
+    }
+  }
+  const post = await getList('api/posts/', query)
+  const postDetails = post.data.length > 0 ? post.data[0] : null
+  return {
+    props: {
+      postDetails
+    }
+  }
+}
 
 export default PostDetails
--- utils/request.ts
+++ utils/request.ts
@@ -1,47 +1,43 @@
 import axios, { AxiosResponse } from "axios"
 import { IStrapiListResponse, IStrapiSingleResponse } from "../interfaces/strapi"
+import qs from 'qs'
 
 type AllowableTypes = 'get' | 'GET' | 'post' | 'POST' | 'put' | 'PUT' | 'PATCH' | 'patch' | 'delete' | 'DELETE'
 interface IRequest {
   url: string
   method: AllowableTypes
-  params?: Record<string, string>
+  params?: Record<string, object>
   body?: object
   headers?: Record<string, string>
 }
 const baseUrl = process.env.BASE_URL
 const request = async <T>({ url, method, params, body, headers } : IRequest) : Promise<AxiosResponse<T>> => {
+  params = params || {}
+  if (method === 'get' && url?.includes('post')) params = { ...params, populate: ['category', 'tags']}
+  const queryString = qs.stringify(params)
+
   headers = headers || {}
   headers = Object.assign(headers, {
     // Authorization: `Bearer ${token}`,
     'Content-Type': 'application/json'
   })
-  params = params || {}
-  const response = await axios(`${baseUrl}/${url}`, {
+
+  const response = await axios(`${baseUrl}/${url}${queryString ? `?${queryString}` : ''}`, {
     method,
     ...(body && { body }),
     headers: headers,
-    params: params
   })
+
   return response
 }
 
-axios.interceptors.request.use((config) => {
-  const { method, url, params } = config
-  if (method === 'get' && url?.includes('posts'))
-  params['populate'] = 'category,tags'
-  return config
-}, (error) => {
-  return Promise.reject(error);
-})
-
-export const getList = async <T>(url: string, params?: Record<string, string>): Promise<IStrapiListResponse<T>> => {
+export const getList = async <T>(url: string, params?: Record<string, object>): Promise<IStrapiListResponse<T>> => {
   const method = 'get'
   const res = await request<IStrapiListResponse<T>>({ url, method, params })
   return res.data
 }
 
-export const get = async <T>(url: string, params?: Record<string, string>): Promise<IStrapiSingleResponse<T>> => {
+export const get = async <T>(url: string, params?: Record<string, object>): Promise<IStrapiSingleResponse<T>> => {
   const method = 'get'
   const res = await request<IStrapiSingleResponse<T>>({ url, params, method })
   return res.data
